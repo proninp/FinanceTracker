737b37403cfb7d0f25d67657af1100c1751d5f2e
proninp <velninpapro@gmail.com>
2025-11-23 19:42:51 +0300

    Updated to .NET 10, uses OpenApi

--- project/backend/FinanceTracker.App/src/FinanceTracker.App/Program.cs
+++ project/backend/FinanceTracker.App/src/FinanceTracker.App/Program.cs
@@ -1,17 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/openapi/v1.json", "Api v1");
+        }
+    );
 }
 
 app.UseHttpsRedirection();
