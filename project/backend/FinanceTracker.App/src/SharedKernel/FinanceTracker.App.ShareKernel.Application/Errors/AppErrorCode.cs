266ab84308c21ce1b2495539d7bb1e78bb79b65c
proninp <velninpapro@gmail.com>
2025-11-26 23:36:38 +0300

    Added Results pattern objects

---
+++ project/backend/FinanceTracker.App/src/SharedKernel/FinanceTracker.App.ShareKernel.Application/Errors/AppErrorCode.cs
@@ -0,0 +1,32 @@
+﻿namespace FinanceTracker.App.ShareKernel.Application.Errors;
+
+/// <summary>
+/// Коды ошибок приложения
+/// </summary>
+public enum AppErrorCode
+{
+    /// <summary>
+    /// Ресурс не найден
+    /// </summary>
+    NotFound,
+
+    /// <summary>
+    /// Доступ запрещен
+    /// </summary>
+    Forbidden,
+
+    /// <summary>
+    /// Ошибка валидации данных
+    /// </summary>
+    ValidationError,
+
+    /// <summary>
+    /// Конфликт данных
+    /// </summary>
+    Conflict,
+
+    /// <summary>
+    /// Непредвиденная ошибка
+    /// </summary>
+    UnexpectedError
+}
---
+++ project/backend/FinanceTracker.App/src/SharedKernel/FinanceTracker.App.SharedKernel.WebApi/Extensions/ResultExtesions.cs
@@ -0,0 +1,39 @@
+﻿using FinanceTracker.App.ShareKernel.Application.Errors;
+using FluentResults;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceTracker.App.SharedKernel.WebApi.Extensions;
+
+public static class ResultExtesions
+{
+    public static IActionResult ToActionResult(this Result result)
+    {
+        throw new  NotImplementedException();
+    }
+
+    private static IActionResult MapErrorToActionResult(AppError error)
+    {
+        var (statusCode, title) = error.ErrorCode switch
+        {
+            AppErrorCode.NotFound => (StatusCodes.Status404NotFound, "Resource Not Found"),
+            AppErrorCode.Forbidden => (StatusCodes.Status403Forbidden, "Forbidden"),
+            AppErrorCode.ValidationError => (StatusCodes.Status400BadRequest, "Validation Error"),
+            AppErrorCode.Conflict => (StatusCodes.Status409Conflict, "Conflict"),
+            _ => (StatusCodes.Status500InternalServerError, "Internal Server Error"),
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = title,
+            Detail = error.Message,
+            Status = statusCode,
+            Type = $"https://httpstatuses.com/{statusCode}"
+        };
+
+        return new ObjectResult(problemDetails)
+        {
+            StatusCode = statusCode,
+        };
+    }
+}
