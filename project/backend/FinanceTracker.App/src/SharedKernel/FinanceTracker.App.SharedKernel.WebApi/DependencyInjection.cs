26af50a7a26cfcaf89b1507e04d76bcbd9e20028
proninp <velninpapro@gmail.com>
2025-12-14 16:11:38 +0300

    Added ILanguageContext DI registration

---
+++ project/backend/FinanceTracker.App/src/SharedKernel/FinanceTracker.App.SharedKernel.WebApi/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using FinanceTracker.App.SharedKernel.WebApi.Localization;
+using FinanceTracker.App.ShareKernel.Application.Localization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FinanceTracker.App.SharedKernel.WebApi;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddSharedKernel(this IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+        services.AddScoped<ILanguageContext, HttpLanguageContext>();
+
+        return services;
+    }
+}
