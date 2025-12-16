88e2ba3b294b24865083d9cb2d35408131d6d863
proninp <velninpapro@gmail.com>
2025-12-16 23:23:15 +0300

    Added GetByIdAsync(), GetByCodeAsync(), GetPagedAsync)( functions to AccountTypeService

--- project/backend/FinanceTracker.App/src/Modules/Accounts/FinanceTracker.App.Accounts.Application/Services/AccountTypeService.cs
+++ project/backend/FinanceTracker.App/src/Modules/Accounts/FinanceTracker.App.Accounts.Application/Services/AccountTypeService.cs
@@ -2,6 +2,7 @@
 using FinanceTracker.App.Accounts.Application.Contracts.Repositories;
 using FinanceTracker.App.Accounts.Application.Contracts.Services;
 using FinanceTracker.App.Accounts.Application.Contracts.UnitOfWork;
+using FinanceTracker.App.ShareKernel.Application.Errors;
 using FinanceTracker.App.ShareKernel.Application.Localization;
 using FinanceTracker.App.ShareKernel.Application.Pagination;
 using FluentResults;
@@ -17,17 +18,39 @@ public sealed class AccountTypeService(
     public async Task<Result<AccountTypeDto?>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
     {
         var accountType = await repository.GetByIdAsync(id, languageContext.CurrentLanguageCode, cancellationToken);
-
-        return Result.Ok(accountType?.ToDto(languageContext.CurrentLanguageCode));
+        var accountTypeDto = accountType?.ToDto(languageContext.CurrentLanguageCode);
+        return accountTypeDto is null
+            ? AppError.NotFound($"Account Type with id: {id} was not found")
+            : Result.Ok();
     }
 
-    public Task<Result<AccountTypeDto?>> GetByCodeAsync(string code, CancellationToken cancellationToken = default)
+    public async Task<Result<AccountTypeDto?>> GetByCodeAsync(string code, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(code))
+        {
+            return AppError.Validation("Account type code cannot be empty");
+        }
+
+        var accountType = await repository.GetByCodeAsync(code, languageContext.CurrentLanguageCode, cancellationToken);
+        var result = accountType?.ToDto(languageContext.CurrentLanguageCode);
+
+        return result is null
+            ? AppError.NotFound($"Account type with code: {code} was not found")
+            : result;
     }
 
-    public Task<Result<PaginationResult<AccountTypeDto>>> GetPagedAsync(PaginationSettings settings, bool includeArchived = false, CancellationToken cancellationToken = default)
+    public async Task<Result<PaginationResult<AccountTypeDto>>> GetPagedAsync(
+        PaginationSettings settings,
+        bool includeArchived = false,
+        CancellationToken cancellationToken = default
+    )
     {
+        var result = await repository.GetPagedAsync(settings, languageContext.CurrentLanguageCode, includeArchived, cancellationToken);
+        var dtoResult = new PaginationResult<AccountTypeDto>(
+            result.Data.Select(at => at.ToDto(languageContext.CurrentLanguageCode)).ToList(),
+            settings,
+            result.TotalCount
+        );
         throw new NotImplementedException();
     }
 
